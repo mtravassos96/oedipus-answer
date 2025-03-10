49ef115702a2a2a30470ae6df6b072f5bc58acc6
Matheus Travassos <mtravassos96@gmail.com>
2025-03-10 19:00:56 -0300

    first tests
    - understanding how math/rand works

---
+++ go.mod
@@ -0,0 +1,3 @@
+module github.com/mtravassos96/oedipus-answer
+
+go 1.23.2
---
+++ main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+func main() {
+	fmt.Println("First tests")
+
+	randomInt := rand.Intn(5) 
+	
+	fmt.Println(randomInt)
+
+	// resource https://www.youtube.com/watch?v=yPeYZ0QuspU
+}
