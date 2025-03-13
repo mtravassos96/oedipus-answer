e9f9b67a851bc2e9762341991f9def733033f5d1
Matheus Travassos <mtravassos96@gmail.com>
2025-03-13 09:35:41 -0300

    Update main.go
    - feat crypto/rand

--- main.go
+++ main.go
@@ -2,15 +2,20 @@ package main
 
 import (
 	"fmt"
-	"math/rand"
-	"time"
+	"math/big"
+	//"math/rand"
+	"crypto/rand"
+	//"time"
 )
 
 func main() {
 	fmt.Println("First tests")
 
 	// Random time method
-	randomTime := rand.New(rand.NewSource(time.Now().UnixNano())) 
-	randomInt := randomTime.Intn(100)
-	fmt.Print(randomInt)
+	// randomTime := rand.New(rand.NewSource(time.Now().UnixNano())) 
+	// randomInt := randomTime.Intn(100)
+	// fmt.Print(randomInt)
+
+	myRandomNum, _ := rand.Int(rand.Reader, big.NewInt(5))
+	fmt.Println(myRandomNum)
 }
